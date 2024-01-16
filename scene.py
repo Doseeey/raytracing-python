fab51b1995752515a3851382c135249d083bec81
Doseeey <jakubdebosz.it@gmail.com>
2024-01-16 09:58:41 +0100

    prepared scene

--- scene.py
+++ scene.py
@@ -18,7 +18,7 @@ class Scene:
                     np.random.uniform(low = 0.3, high = 1.0),
                     np.random.uniform(low = 0.3, high = 1.0)
                 ],
-                prop = np.random.randint(2)
+                prop = 0 if np.random.uniform(low = 0, high = 1) < 0.8 else 1
             ) for i in range(128)
         ]
 
@@ -31,17 +31,47 @@ class Scene:
                 normal = [0, 0, 1],
                 tangent= [1, 0, 0],
                 bitangent= [0, 1, 0],
-                uMin = -200,
-                uMax = 200,
-                vMin = -200,
-                vMax = 200,
+                uMin = -20,
+                uMax = 20,
+                vMin = -20,
+                vMax = 20,
                 center = [0, 0, -20],
                 color = [
                     np.random.uniform(low = 0.3, high = 1.0),
                     np.random.uniform(low = 0.3, high = 1.0),
                     np.random.uniform(low = 0.3, high = 1.0)
                 ]
-            )
+            ),
+            plane.Plane(
+                normal = [1, 0, 0],
+                tangent= [0, 1, 0],
+                bitangent= [0, 0, 1],
+                uMin = -20,
+                uMax = 20,
+                vMin = -20,
+                vMax = 20,
+                center = [-20, 0, 0],
+                color = [
+                    np.random.uniform(low = 0.3, high = 1.0),
+                    np.random.uniform(low = 0.3, high = 1.0),
+                    np.random.uniform(low = 0.3, high = 1.0)
+                ]
+            ),
+            plane.Plane(
+                normal = [1, 0, 0],
+                tangent= [0, 1, 0],
+                bitangent= [0, 0, 1],
+                uMin = -50,
+                uMax = 50,
+                vMin = -50,
+                vMax = 50,
+                center = [150, 0, 0],
+                color = [
+                    np.random.uniform(low = 0.3, high = 1.0),
+                    np.random.uniform(low = 0.3, high = 1.0),
+                    np.random.uniform(low = 0.3, high = 1.0)
+                ]
+            ),
         ]
 
         self.outDated = True
